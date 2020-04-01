a144a4783fc1d1803ad2bcca53df72c13eadde91
hannesbachmann <44273217+hannesbachmann@users.noreply.github.com>
2020-04-01 21:16:13 +0200

    Add files via upload
    add file double linked list

---
+++ double_linkedlist.c
@@ -0,0 +1,100 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// single element with its own value and a pointer on the next element in the list
+typedef struct element {
+    int value;
+    struct element_t *previous;
+    struct element_t *next;
+} element_t;
+
+// whole linkedlist with head-element (first element in list) and size (length of the list)
+typedef struct double_linkedlist {
+    element_t *head;
+    size_t size;
+} double_linkedlist_t;
+
+double_linkedlist_t *create_emptylist();
+void append(double_linkedlist_t *mylist, int myvalue);
+void remove_element(double_linkedlist_t *mylist, int myindex);
+int get(double_linkedlist_t *mylist, int myindex);
+void delete_doublelinkedlist(double_linkedlist_t *mylist);
+
+int main(){
+    double_linkedlist_t *new_doublelinkedlist = create_emptylist();
+
+    append(new_doublelinkedlist, 10);
+    append(new_doublelinkedlist, 20);
+    append(new_doublelinkedlist, 11);
+    append(new_doublelinkedlist, 11);
+    append(new_doublelinkedlist, 11);
+
+    remove_element(new_doublelinkedlist, 1);
+
+    delete_doublelinkedlist(new_doublelinkedlist);
+
+    int some_value = get(new_doublelinkedlist, 1);
+    printf("%d hello\n", some_value);
+
+    return 0;
+}
+
+double_linkedlist_t *create_emptylist(){
+    double_linkedlist_t *newlist_ptr = malloc(sizeof(double_linkedlist_t));
+    newlist_ptr->size = 0;
+    newlist_ptr->head = NULL;
+
+    return newlist_ptr;
+}
+
+void append(double_linkedlist_t *mylist, int myvalue){
+    element_t *newelement_ptr = malloc(sizeof(element_t));
+    newelement_ptr->value = myvalue;
+    newelement_ptr->next = NULL;
+
+    if (mylist->head == NULL){
+        mylist->head = newelement_ptr;
+        newelement_ptr->previous = NULL;
+    }else{
+        element_t *last_element = mylist->head;
+        while (last_element->next != NULL){
+            last_element = last_element->next;
+        }
+        newelement_ptr->previous = last_element;
+        last_element->next = newelement_ptr;
+    }
+    mylist->size += 1;
+}
+
+void remove_element(double_linkedlist_t *mylist, int myindex){
+    element_t *last_element = mylist->head;
+    for (int index = 0; index<(myindex - 1); index++){
+        last_element = last_element->next;
+    }
+    element_t *tmp_element = last_element->next;
+    last_element->next = tmp_element->next;
+    last_element = last_element->next;
+    last_element->previous = tmp_element->previous;
+    free(tmp_element);
+    mylist->size -= 1;
+}
+
+int get(double_linkedlist_t *mylist, int myindex){
+    element_t *last_element = mylist->head;
+    for (int index = 0; index<(myindex); index++){
+        last_element = last_element->next;
+    }
+
+    return last_element->value;
+}
+
+void delete_doublelinkedlist(double_linkedlist_t *mylist){
+    element_t *last_element = mylist->head;
+    while (last_element->next != NULL){
+        element_t *tmp_element = last_element->next;
+        last_element = tmp_element;
+        free(tmp_element);
+    }
+    free(mylist->head);
+    free(mylist);
+}
