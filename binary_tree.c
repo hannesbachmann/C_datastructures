e762a312e07a56b42523f19c6f8a42645dcbb15a
hannesbachmann <44273217+hannesbachmann@users.noreply.github.com>
2020-04-02 23:24:41 +0200

    Update binary_tree.c
    added the head of the delete_bintree_element() method

--- binary_tree.c
+++ binary_tree.c
@@ -19,6 +19,7 @@ element_t *recursive_search(element_t *myelement, int myvalue);
 void delete_element(bin_tree_t *mytree, int myvalue);
 element_t *search_for_parent(element_t *root_element, element_t *child_element);
 void delete_bintree(bin_tree_t *mytree);
+void delete_bintree_element(element_t *root_element);
 
 
 bin_tree_t *create_bintree(){
